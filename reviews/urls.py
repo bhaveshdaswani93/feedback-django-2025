cdb92322fd502cae076a281cb4a63dbe0b3f373a
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-02-20 14:04:17 +0530

    Implement review submission and thank you page

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.reviews)
+   path('', views.reviews),
+   path('thank_you', views.thank_you)
   ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
 def reviews(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    print(username)
+    return HttpResponseRedirect('/reviews/thank_you')
+
   return render(request, 'reviews/review.html')
+
+def thank_you(request):
+  return render(request, 'reviews/thank_you.html')
