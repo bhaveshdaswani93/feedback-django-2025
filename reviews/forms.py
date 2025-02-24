f68bcb5e320f8c0bfcc36e4f05a56c2e5ee3df7a
Bhavesh <bhaveshdaswani93@gmail.com>
2025-02-24 13:37:57 +0530

    working on Model form

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from .models import Review
+
+"""
 class ReviewForm(forms.Form):
   user_name = forms.CharField(label='Your Name', max_length=100, error_messages={
     'required': 'Your name must not be empty',
@@ -12,5 +15,24 @@ class ReviewForm(forms.Form):
     'min_value': 'Please enter a rating of 1 or higher',
     'max_value': 'Please enter a rating of 5 or lower'
   })
+  """
+  
+class ReviewForm(forms.ModelForm):
+  class Meta:
+    model = Review
+    fields = ('user_name', 'review', 'rating')
+    labels = {
+      'user_name': 'Your name',
+      'review': 'Your Review',
+      'rating': 'Please Enter rating'
+    }
+    error_messages = {
+      'user_name': {
+        'required': 'User name is required'
+      }
+    }
+    
+  
+    
   
   
