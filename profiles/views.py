5f8f7127f9560060a7ae699e557b2e9aca658ce8
Bhavesh <bhaveshdaswani93@gmail.com>
2025-02-27 13:36:25 +0530

    working on file upload

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
-
+def file_upload_util(file):
+  with open('temp/image.jpg', "wb+") as dest:
+    for chunk in file.chunks():
+      dest.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        print(request.FILES['image'])
+        return HttpResponseRedirect('/profiles')
