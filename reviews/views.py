c73734d848da5f46b45a37e2d9ff713d232092e5
Bhavesh <bhaveshdaswani93@gmail.com>
2025-03-02 23:11:23 +0530

    fix for session get

--- reviews/views.py
+++ reviews/views.py
@@ -100,7 +100,7 @@ class ReviewDetailView(DetailView):
   def get_context_data(self, **kwargs):
     context =  super().get_context_data(**kwargs)
     review = self.object
-    favorite_review = self.request.session['favorite_review']
+    favorite_review = self.request.session.get('favorite_review')
     context['is_favorite'] = favorite_review == str(review.pk)
   """
   def get_context_data(self, **kwargs):
